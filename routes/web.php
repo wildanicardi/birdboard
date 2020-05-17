213b20e089983d608f210c8e4d9c23328885f8a1
wildanicardi <wildanicardi9@gmail.com>
2020-05-18 01:02:50 +0700

    project create testing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/projects', 'ProjectController@index');
+Route::post('/projects', 'ProjectController@store');
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test **/
+    public function a_user_can_create_a_project()
+    {
+        $this->withExceptionHandling();
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+        // $this->assertDatabaseHas('projects', $attributes);
+
+        // $this->get('/projects')->assertSee($attributes['title']);
+    }
+}
